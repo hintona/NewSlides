204c76a13633f2ae2817a39f14eb0d7e52b8ef47
KarlD7 <64757701+KarlD7@users.noreply.github.com>
2021-03-07 23:31:27 -0500

    Home Page and Animations

--- NewSlides/NewSlides.py
+++ NewSlides/NewSlides.py
@@ -7,19 +7,45 @@ from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from kivy.uix.button import Button
+from kivy.base import runTouchApp
+from kivy.animation import Animation
+from kivy.uix.screenmanager import Screen, ScreenManager
 
-Builder.load_file("venv/NewSlides.kv")
+#ns = Builder.load_file("NewSlides.kv")
 
+class WindowManager(ScreenManager):
+    def buttonAnimInit(self, widget):
+        animate = Animation(background_color=(95 / 255, 158 / 255, 160 / 255, 1), duration=.1)
+        animate.start(widget)
 
-class MyLayout(Widget):
+    def buttonAnimFin(self, widget):
+        animate = Animation(background_color=(0, 0, 0, 0), duration=.1)
+        animate.start(widget)
+
+class HomeWindow(Screen, Widget):
+    def buttonAnimInit(self, widget):
+        animate = Animation(background_color=(95 / 255, 158 / 255, 160 / 255, 1), duration=.1)
+        animate.start(widget)
+
+    def buttonAnimFin(self, widget):
+        animate = Animation(background_color=(0, 0, 0, 0), duration=.1)
+        animate.start(widget)
+
+class MainWindow(Screen, Widget):
     def newSlide(self):
         grid = self.ids.slides
-        grid.add_widget(Button(text="1"))
-
+        grid.add_widget(Button(text="Slide", size_hint_y=None, height=200, background_color=(0,0,.1,.1), color=(0,0,.9,1)))
+    def buttonAnimInit(self, widget):
+        animate = Animation(background_color=(95/255,158/255,160/255,1), duration=.1)
+        animate.start(widget)
+    def buttonAnimFin(self, widget):
+        animate = Animation(background_color=(0, 0, 0, 0), duration=.1)
+        animate.start(widget)
 class MyApp(App):
     def build(self):
         Window.maximize()
         Window.clearcolor = (1,1,1,1)
-        return MyLayout()
+        ns = Builder.load_file("NewSlides.kv")
+        return ns
 
 MyApp().run()
