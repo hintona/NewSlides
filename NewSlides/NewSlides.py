bd8071ec98f34147633f6a740a895d160b599f42
KarlD7 <64757701+KarlD7@users.noreply.github.com>
2021-03-11 19:53:57 -0500

    Final GUI Design, can delete slides and have them not count toward slide count, added icons for all buttons

--- NewSlides/NewSlides.py
+++ NewSlides/NewSlides.py
@@ -10,42 +10,70 @@ from kivy.uix.button import Button
 from kivy.base import runTouchApp
 from kivy.animation import Animation
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.image import Image
+from kivy.uix.checkbox import CheckBox
+import copy
 
 #ns = Builder.load_file("NewSlides.kv")
-
 class WindowManager(ScreenManager):
-    def buttonAnimInit(self, widget):
-        animate = Animation(background_color=(95 / 255, 158 / 255, 160 / 255, 1), duration=.1)
-        animate.start(widget)
-
-    def buttonAnimFin(self, widget):
-        animate = Animation(background_color=(0, 0, 0, 0), duration=.1)
-        animate.start(widget)
+    pass
 
 class HomeWindow(Screen, Widget):
-    def buttonAnimInit(self, widget):
-        animate = Animation(background_color=(95 / 255, 158 / 255, 160 / 255, 1), duration=.1)
-        animate.start(widget)
+    pass
 
-    def buttonAnimFin(self, widget):
-        animate = Animation(background_color=(0, 0, 0, 0), duration=.1)
-        animate.start(widget)
+class SlideView(GridLayout, Widget):
+    def getSlideCount(self):
+        return str(numSlides)
+
+    def removeSlide(self):
+        global numSlides
+        slideView = self.getCountDisplay()
+        grid = self.parent
+        grid.remove_widget(self)
+        numSlides -= 1
+        slideView.text = "Slide Count: " + str(numSlides)
+        for i in range(len(grid.children)):
+            grid.children[len(grid.children)-i-1].ids.count.text = str(i+1)
+
+    def duplicateSlide(self, SlideView):
+        newSlide = copy.deepcopy(SlideView)
+        newSlide.parent = None
+        grid = self.parent
+        grid.add_widget(newSlide)
+
+    def changeSlideCount(self, checkbox):
+        slideView = self.getCountDisplay()
+        global numSlides
+        if(checkbox.active):
+            numSlides += 1
+            slideView.text = "Slide Count: " + str(numSlides)
+        else:
+            numSlides -= 1
+            slideView.text = "Slide Count: " + str(numSlides)
+
+    def getCountDisplay(self):
+        slideView = self.parent.parent.parent.parent.parent.parent.parent.ids.slideCount
+        return slideView
+
 
 class MainWindow(Screen, Widget):
     def newSlide(self):
         grid = self.ids.slides
-        grid.add_widget(Button(text="Slide", size_hint_y=None, height=200, background_color=(0,0,.1,.1), color=(0,0,.9,1)))
-    def buttonAnimInit(self, widget):
-        animate = Animation(background_color=(95/255,158/255,160/255,1), duration=.1)
-        animate.start(widget)
-    def buttonAnimFin(self, widget):
-        animate = Animation(background_color=(0, 0, 0, 0), duration=.1)
-        animate.start(widget)
+        count = self.ids.slideCount
+        global numSlides
+        numSlides += 1
+        grid.add_widget(SlideView())
+        count.text = "Slide Count: " + str(numSlides)
+
 class MyApp(App):
     def build(self):
+        self.title = "New Slides"
+        #self.main = MyApp.get_running_app()
+        #print(self.main.root)
         Window.maximize()
-        Window.clearcolor = (1,1,1,1)
+        Window.clearcolor = (37/255,14/255,98/255,1)
         ns = Builder.load_file("NewSlides.kv")
         return ns
 
+numSlides = 0
 MyApp().run()
