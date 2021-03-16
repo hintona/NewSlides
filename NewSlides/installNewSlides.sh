5e71a3dd324c392ae0a7be80edf6c0212238d362
Alex Hinton <64749723+hintona@users.noreply.github.com>
2021-03-16 04:01:29 +0000

    Added line to install fpdf package

--- NewSlides/installNewSlides.sh
+++ NewSlides/installNewSlides.sh
@@ -4,6 +4,7 @@ sudo apt update
 sudo apt install pkg-config libgl1-mesa-dev libgles2-mesa-dev \ libgstreamer1.0-dev \ gstreamer1.0-plugins-{bad,base,good,ugly} \ gstreamer1.0-{omx,alsa} libmtdev-dev \ xclip xsel libjpeg-dev
 sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev
 python3 -m pip install kivy[base] kivy_examples
+python3 pip install fpdf
 echo "Dependencies installed"
 
 #Creates desktop shortcut
